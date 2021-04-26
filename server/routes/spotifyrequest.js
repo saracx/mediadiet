4c848fe5fde53109b84d341f4b48c5d7b7729cf1
sara <saracx@pm.me>
2021-04-26 20:06:29 +0200

    Clear creds

--- server/routes/spotifyrequest.js
+++ server/routes/spotifyrequest.js
@@ -2,11 +2,9 @@ const express = require("express");
 const router = express.Router();
 var request = require("request");
 var postQuery = "grant_type=client_credentials";
+const { CLIENT_ID, CLIENT_SECRET } = require("../../secrets.json");
 
 const getToken = async (req, res) => {
-    const CLIENT_ID = "2791591419614dfc8a6d0f0d28c9063c";
-    const CLIENT_SECRET = "2696c842efc64343aee933784e43ab94";
-
     const creds = `${CLIENT_ID}:${CLIENT_SECRET}`;
     const encodedCreds = btoa(creds);
     console.log("arrived at gettoken");
